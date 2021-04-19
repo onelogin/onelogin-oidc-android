8589bebb5ee82c9fb5a3aa347236d80658bc9f43
Alex Queudot <Alqueraf@gmail.com>
2021-04-19 15:10:20 +0200

    Update UserInfo.kt
    - Add snake_case SerializedName for multiple word parameters.
    - Set updatedAt type to Int.

--- oneloginoidc/src/main/java/com/onelogin/oidc/userInfo/UserInfo.kt
+++ oneloginoidc/src/main/java/com/onelogin/oidc/userInfo/UserInfo.kt
@@ -3,10 +3,14 @@ package com.onelogin.oidc.userInfo
 data class UserInfo(
     val sub: String,
     val email: String,
+    @SerializedName("preferred_username")
     val preferredUsername: String?,
     val name: String?,
-    val updatedAt: String?,
+    @SerializedName("updated_at")
+    val updatedAt: Int?,
+    @SerializedName("given_name")
     val givenName: String?,
+    @SerializedName("family_name")
     val familyName: String?,
     val groups: List<String>?
 )
